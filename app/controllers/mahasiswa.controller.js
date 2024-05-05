5afd1395d19b7fc3fe25fa10a216863fdcd21699
George Miracle <97882379+georgemiraclee@users.noreply.github.com>
2024-05-05 23:22:11 +0700

    controller delete

--- app/controllers/mahasiswa.controller.js
+++ app/controllers/mahasiswa.controller.js
@@ -29,16 +29,25 @@ exports.update = (req, res) => {
     req.body.tanggal_lahir = new Date(req.body.tanggal_lahir)
 
     mahasiswa.findByIdAndUpdate(id, req.body, {useFindAndModify: false})
-    .then(data =>{
-        if(!data){
-            res.status(404).send({message: `data tidak ditemukan dengan id: ${id}`
-            })
-        }else{
-            res.send({message: "data berhasil di update"})
+    .then(data => {
+        if (!data) {
+            res.status(404).send({message: `data tidak ditemukan dengan id: ${id}`})
+        } else {
+            res.send({message: "data berhasil diupdate"})
         }
     })
+    
     .catch(err => res.status(500).send({message: err.message}));
 }
 exports.delete = (req, res) => {
-    
+    const id = req.params.id;
+    mahasiswa.findByIdAndDelete(id)
+    .then(data => {
+        if (!data) {
+            res.status(404).send({message: `data tidak ditemukan dengan id: ${id}`});
+        } else {
+            res.send({message: "data berhasil dihapus"});
+        }
+    })
+    .catch(err => res.status(500).send({message: err.message}));
 }
